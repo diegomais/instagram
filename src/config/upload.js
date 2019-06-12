c591ead2372822e27212e83603bbc8901104e1cc
Diego Mais <diegomais@live.com>
2019-06-12 00:33:42 -0300

    Add strong pseudo-random data to provide a unique filename.
    - This feature generates cryptographically strong pseudo-random data to provide a unique filename.
    
    Changes to be committed:
    	modified:   src/config/upload.js

--- src/config/upload.js
+++ src/config/upload.js
@@ -4,6 +4,8 @@ const multer = require('multer');
 // Import path module: utilities for working with file and directory paths.
 const path = require('path');
 
+const crypto = require('crypto');
+
 module.exports = {
   // storage: Where to store the files. The diskStorage engine gives full control on storing files to disk.
   storage: multer.diskStorage({
@@ -11,7 +13,12 @@ module.exports = {
     destination: path.resolve(__dirname, '..', '..', 'uploads'),
     // filename is used to determine what the file should be named inside the folder.
     filename: function(req, file, cb) {
-      cb(null, file.originalname);
+      // Generates cryptographically strong pseudo-random data to provide a unique filename.
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) throw err;
+        file.key = `${hash.toString('hex')}-${file.originalname}`;
+        cb(null, file.key);
+      });
     }
   })
 };
