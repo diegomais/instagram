4025ffaf800dd9053c343448d12fd26719542abf
Diego Mais <diegomais@live.com>
2019-06-13 23:03:48 -0300

    Add resize and convert image to JPEG output
    - This feature provides to resize and convert uploaded image to JPEG output to prevent large and heavy files.
    
    Changes to be committed:
    	modified:   package.json
    	modified:   yarn.lock
    	modified:   src/controllers/PostController.js

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,3 +1,12 @@
+// Import sharp package: High performance Node.js image processing, the fastest module to resize JPEG, PNG, WebP and TIFF images.
+const sharp = require('sharp');
+
+// Import path module: utilities for working with file and directory paths.
+const path = require('path');
+
+// Import file system module: interact with the file system in a manner closely modeled around standard POSIX functions.
+const fs = require('fs');
+
 // Load Post model.
 const Post = require('../models/Post');
 
@@ -21,13 +30,28 @@ module.exports = {
     const { author, place, description, hashtags } = req.body;
     const { filename: image } = req.file;
 
+    // Destructuring name and extension of file.
+    const [name] = image.split('.');
+
+    // Append ".jpg" to name of file
+    const fileName = `${name}.jpg`;
+
+    // Resize uploaded image to 500px wide,convert to JPEG output and save to resized folder.
+    await sharp(req.file.path)
+      .resize(500)
+      .jpeg({ quality: 70 })
+      .toFile(path.resolve(req.file.destination, 'resized', fileName));
+
+    // Remove uploaded image.
+    fs.unlinkSync(req.file.path);
+
     // Insert values on database.
     const post = await Post.create({
       author,
       place,
       description,
       hashtags,
-      image
+      image: fileName
     });
 
     // Return post saved on database.
