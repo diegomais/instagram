f34bd54f7d8886c8fa67b577c160f2346ad05ee4
Diego Mais <diegomais@live.com>
2019-06-14 23:26:04 -0300

    Add header component
    - This feature provides header to user navigate in the app.
    
    Changes to be committed:
    	new file:   src/assets/logo.svg
    	new file:   src/assets/camera.svg
    	new file:   src/components/Header.js
    	new file:   src/components/Header.css
    	modified:   src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import Header from './components/Header';
 import Routes from './routes';
 
 import './App.css';
@@ -8,6 +9,7 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />;
     </BrowserRouter>
   );
