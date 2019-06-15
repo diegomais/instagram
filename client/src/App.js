0547f7db53a35d5610807eea1485c0e0f724572e
Diego Mais <diegomais@live.com>
2019-06-15 00:48:56 -0300

    Add dynamic content to feed component
    - This feature provides user to see the content in database on feed component.
    
    Changes to be committed:
    	modified:   package.json
    	modified:   yarn.lock
    	modified:   src/App.js
    	modified:   src/pages/Feed.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes />;
+      <Routes />
     </BrowserRouter>
   );
 }
--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -1,5 +1,5 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import './Feed.css';
 
@@ -8,65 +8,43 @@ import like from '../assets/like.svg';
 import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
-const Feed = props => {
+const Feed = () => {
+  const [feed, setFeed] = useState([]);
+  useEffect(() => {
+    const fetchFeed = async () => {
+      const res = await axios.get('http://localhost:3333/api/posts');
+      setFeed(res.data);
+    };
+    fetchFeed();
+  }, []);
   return (
     <section id='post-list'>
-      <article>
-        <header>
-          <div className='user-info'>
-            <span>Diego Mais</span>
-            <span className='place'>Brazil</span>
-          </div>
-          <img src={more} alt='More' />
-        </header>
-        <img
-          src='http://localhost:3333/files/f3101e4baf7ac3a7b9c8516bb1c177b5-logo.jpg'
-          alt='Post'
-        />
-        <footer>
-          <div className='actions'>
-            <img src={like} alt='Like' />
-            <img src={comment} alt='Comment' />
-            <img src={send} alt='Send' />
-          </div>
-          <strong>900 likes</strong>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
-            vero.
-            <span>#react #node #top</span>
-          </p>
-        </footer>
-      </article>
-      <article>
-        <header>
-          <div className='user-info'>
-            <span>Diego Mais</span>
-            <span className='place'>Brazil</span>
-          </div>
-          <img src={more} alt='More' />
-        </header>
-        <img
-          src='http://localhost:3333/files/f3101e4baf7ac3a7b9c8516bb1c177b5-logo.jpg'
-          alt='Post'
-        />
-        <footer>
-          <div className='actions'>
-            <img src={like} alt='Like' />
-            <img src={comment} alt='Comment' />
-            <img src={send} alt='Send' />
-          </div>
-          <strong>900 likes</strong>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
-            vero.
-            <span>#react #node #top</span>
-          </p>
-        </footer>
-      </article>
+      {feed.map(post => (
+        <article key={post._id}>
+          <header>
+            <div className='user-info'>
+              <span>{post.author}</span>
+              <span className='place'>{post.place}</span>
+            </div>
+            <img src={more} alt='More' />
+          </header>
+          <img src={`http://localhost:3333/files/${post.image}`} alt='Post' />
+          <footer>
+            <div className='actions'>
+              <img src={like} alt='Like' />
+              <img src={comment} alt='Comment' />
+              <img src={send} alt='Send' />
+            </div>
+            <strong>{post.likes} likes</strong>
+            <p>
+              {post.description}
+              <span>{post.hashtags}</span>
+            </p>
+          </footer>
+        </article>
+      ))}
     </section>
   );
 };
-
-// Feed.propTypes = {};
 
 export default Feed;
