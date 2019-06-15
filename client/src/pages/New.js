8df15bdd0144d248bd4a6d429165942d8fcf9f09
Diego Mais <diegomais@live.com>
2019-06-15 01:26:00 -0300

    Add new post component
    - This feature provides user to send a new post.
    
    Changes to be committed:
    	modified:   src/pages/New.js
    	new file:   src/pages/New.css

--- client/src/pages/New.js
+++ client/src/pages/New.js
@@ -1,14 +1,81 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import axios from 'axios';
+
+import './New.css';
 
 const New = props => {
+  const [formData, setFormData] = useState({
+    image: null,
+    author: '',
+    place: '',
+    description: '',
+    hashtags: ''
+  });
+
+  const { image, author, place, description, hashtags } = formData;
+
+  const handleImageChange = e =>
+    setFormData({ ...formData, image: e.target.files[0] });
+
+  const handleChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    // Creating a FormData object to send file
+    const data = new FormData();
+
+    // HTML file input, chosen by user
+    data.append('image', image);
+    data.append('author', author);
+    data.append('place', place);
+    data.append('description', description);
+    data.append('hashtags', hashtags);
+
+    // Send data to database
+    await axios.post('http://localhost:3333/api/posts', data);
+
+    // Redirect user to root
+    props.history.push('/');
+  };
+
   return (
     <div>
-      <h1>New</h1>
+      <form id='new-post' onSubmit={e => handleSubmit(e)}>
+        <input type='file' onChange={e => handleImageChange(e)} />
+        <input
+          type='text'
+          placeholder='Author'
+          name='author'
+          value={author}
+          onChange={e => handleChange(e)}
+        />
+        <input
+          type='text'
+          placeholder='Place'
+          name='place'
+          value={place}
+          onChange={e => handleChange(e)}
+        />
+        <input
+          type='text'
+          placeholder='Description'
+          name='description'
+          value={description}
+          onChange={e => handleChange(e)}
+        />
+        <input
+          type='text'
+          placeholder='Hashtags'
+          name='hashtags'
+          value={hashtags}
+          onChange={e => handleChange(e)}
+        />
+        <button type='submit'>Send</button>
+      </form>
     </div>
   );
 };
-
-// New.propTypes = {};
 
 export default New;
