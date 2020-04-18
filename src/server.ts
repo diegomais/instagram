bcfc03aaab10625db4cedddd2497a139c140b7e6
Diego Mais <diegomais.dev@gmail.com>
2020-04-17 23:24:02 -0300

    style: Add ESLint and set up configuration file ESLint is a tool for identifying and reporting on patterns found in ECMAScript/JavaScript code, with the goal of making code more consistent and avoiding bugs.
    Changes to be committed:
    	new file:   .eslintrc.json
    	modified:   .gitignore
    	modified:   package.json
    	modified:   src/server.ts
    	modified:   tsconfig.json
    	modified:   yarn.lock

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,7 @@ const app = express();
 
 const port = 3333;
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello  World' });
-});
+app.get('/', (request, response) => response.json({ message: 'Hello  World' }));
 
 app.listen(port, () => {
   console.log(`🚀 Server started on port ${port}!`);
