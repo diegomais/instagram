765d873bc6d6c7b508cbd26bbf8157f302178914
Diego Mais <diegomais.dev@gmail.com>
2020-04-17 23:35:42 -0300

    style: Add eslint-import-resolver-typescript This plugin adds TypeScript support to eslint-plugin-import.
    Changes to be committed:
    	modified:   .eslintrc.json
    	modified:   package.json
    	new file:   src/routes/index.ts
    	modified:   src/server.ts
    	modified:   yarn.lock

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 const port = 3333;
 
-app.get('/', (request, response) => response.json({ message: 'Hello  World' }));
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`🚀 Server started on port ${port}!`);
