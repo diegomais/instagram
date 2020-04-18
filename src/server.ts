56b0c06b35869dc585d29adf7b0b0e96f4452276
Diego Mais <diegomais.dev@gmail.com>
2020-04-17 21:51:42 -0300

    Add ts-node-dev
    It restarts target node process when any of required files changes (as standard node-dev) but shares Typescript compilation process between restarts. This significantly increases speed of restarting comparing to node-dev -r ts-node/register ..., nodemon -x ts-node ... variations because there is no need to instantiate ts-node compilation each time.
    
    Changes to be committed:
    	deleted:    dist/services/CreateUser.js
    	modified:   package.json
    	deleted:    src/index.ts
    	deleted:    src/routes.ts
    	new file:   src/server.ts
    	deleted:    src/services/CreateUser.ts
    	modified:   tsconfig.json
    	modified:   yarn.lock

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const port = 3333;
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello  World' });
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Server started on port ${port}!`);
+});
